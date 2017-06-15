bc5d8e429d3696864a7a2d58ecd727d26986ab68
ArTex <avqe@ya.ru>
2017-06-16 00:12:42 +0300

    Feedback

--- medtus/ajax.py
+++ medtus/ajax.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-__author__ = 'PekopT'
 
 from dajax.core import Dajax
 from dajaxice.decorators import dajaxice_register
-
+from settings.views import send_email
+from django.conf import settings
+from django.core.mail import send_mail
+from .models import Feedback
 
 @dajaxice_register
 def feedback(request, topic, name, position, contact, text):
@@ -13,18 +15,17 @@ def feedback(request, topic, name, position, contact, text):
         dajax.alert("Необходимо заполнить все поля!")
         return dajax.json()
 
-    from models import Feedback
     message = Feedback(topic=topic, name=name, text=text, contact=contact, position=position)
     message.save()
 
     if message.pk:
-        from settings.views import send_email
-        from account.models import MyUser
-        to = [u.email for u in MyUser.objects.filter(is_admin=True, is_active=True)]
-        for email in to:
-            send_email(request, email, 'feedback_email', u"Новое сообщение обратной связи", message=message)
+        try:
+            send_mail(u"Новое сообщение обратной связи", message, 'feedback_email', settings.COORD_EMAIL, fail_silently=False)
+            dajax.alert(u'Спасибо! Сообщение отправлено. Мы обязательно ответим Вам.')
+        except:
+            dajax.alert(u'При отправке произошла ошибка - проверьте правильность emails')
+
         dajax.script("$('#message').click();")
-        dajax.alert('Спасибо! Сообщение отправлено. Мы обязательно ответим Вам.')
     else:
         dajax.alert('Что-то пошло не так. Попробуйте позже.')
 
