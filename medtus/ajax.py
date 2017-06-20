4748a6cd9b7ea6d8592ae8a5ca49ad4dc6303c27
ArTex <avqe@ya.ru>
2017-06-21 00:58:12 +0300

    email feedbacks

--- medtus/ajax.py
+++ medtus/ajax.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 from .models import Feedback
 from django.template.loader import render_to_string
+import json
 
 @dajaxice_register
 def feedback(request, topic, name, position, contact, text):
@@ -18,14 +19,21 @@ def feedback(request, topic, name, position, contact, text):
 
     message = Feedback(topic=topic, name=name, text=text, contact=contact, position=position)
     message.save()
-
+    context ={
+    'topic': topic,
+    'name': name,
+    'text': text,
+    'contact': contact,
+    'position': position
+    }
+    msg=render_to_string('emails/feedback_email.html', context)
     if message.pk:
         try:
-            send_mail(u"Новое сообщение обратной связи", render_to_string('emails/feedback_email.html', message), 'feedback@vrvm.ru', [settings.COORD_EMAIL], fail_silently=True)
+            send_mail(u"Новое сообщение обратной связи", msg, 'feedback@vrvm.ru', ['artexmail@gmail.com'], fail_silently=True)
             dajax.alert(u'Спасибо! Сообщение отправлено. Мы обязательно ответим Вам.')
             dajax.script("$('#message').click();")
         except:
-            dajax.alert(u'Что-то пошло не так. Попробуйте позже.')
+            dajax.alert(u'Что-то пошло не так при отправке. Попробуйте позже.')
 
     else:
         dajax.alert('Что-то пошло не так. Попробуйте позже.')
