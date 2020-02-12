63eea360dc66b0d505ae91fb23ed94f5a6254eb8
ArTex <avqe@ya.ru>
2020-02-12 18:25:53 +0300

    Verbose reports

--- account/views.py
+++ account/views.py
@@ -397,7 +397,10 @@ def allmsg(request, **kwargs):
         msg.datetime=str(msg.datetime)+str(timezone.localtime(timezone.now()).strftime("%Y-%m-%d %H:%M:%S"))+'|'
         msg.type=0
         msg.save()
-        file_content+=str(i+1)+u'. '+str(user.login.encode('ascii', 'ignore').decode('ascii'))+u' - доставлено\n'
+        file_content+=str(i+1)+u'. '+str(user.login.encode('ascii', 'ignore').decode('ascii'))+u', '+str(user.firstname.encode('ascii', 'ignore').decode('ascii'))+u' '+\
+                      str(user.lastname.encode('ascii', 'ignore').decode('ascii'))+u', '+\
+                      str(user.spec_id.name.encode('ascii', 'ignore').decode('ascii'))+u', '+str(user.town.name.encode('ascii', 'ignore').decode('ascii'))+u', '+\
+                      str(user.country.name.encode('ascii', 'ignore').decode('ascii'))+u' - доставлено\n'
     f = open(os.path.join(settings.STATIC_ROOT,
                            'sending-'+str(timezone.localtime(timezone.now()).strftime("%Y-%m-%d %H:%M:%S"))+'.txt'), 'w+')                   
     f.write(file_content.encode("utf-16"))
@@ -451,7 +454,11 @@ def allmsgcodes(request, **kwargs):
         msg.datetime=str(msg.datetime)+str(timezone.localtime(timezone.now()).strftime("%Y-%m-%d %H:%M:%S"))+'|'
         msg.type=1        
         msg.save()
-        file_content+=str(i+1)+u'. '+str(user.login.encode('ascii', 'ignore').decode('ascii'))+u' код:'+codes[i]+u' - доставлено\n'
+        file_content+=str(i+1)+u'. '+str(user.login.encode('ascii', 'ignore').decode('ascii'))+u', '+str(user.firstname.encode('ascii', 'ignore').decode('ascii'))+u' '+\
+                      str(user.lastname.encode('ascii', 'ignore').decode('ascii'))+u', '+\
+                      str(user.spec_id.name.encode('ascii', 'ignore').decode('ascii'))+u', '+str(user.town.name.encode('ascii', 'ignore').decode('ascii'))+u', '+\
+                      str(user.country.name.encode('ascii', 'ignore').decode('ascii'))+\
+                      u' код:'+codes[i]+u' - доставлено\n'
     f = open(os.path.join(settings.STATIC_ROOT,
                           'sending-NMO.log-'+str(timezone.localtime(timezone.now()).strftime("%Y-%m-%d %H:%M:%S"))+'.txt'), 'w+')
     f.write(file_content.encode("utf-16"))
